1b7a3e5d2e0bfb87ffdf4f9ab0ecc5877fdab8c5
Mitch Anderson <67846917+MitchA29@users.noreply.github.com>
2022-01-02 22:02:20 -0600

    Small styling details throughout application.

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,29 +1,14 @@
 import React from 'react';
 import "./LandingPage.css"
-import { NavLink } from "react-router-dom";
 import axios from 'axios';
-import { Link } from "react-router-dom";
 
 const LandingPage = (props)=>{
-    let handleSubmitLogIn = async (event) => {
-        event.preventDefault();
-        let response = await axios.post("http://127.0.0.1:8000/api/auth/login");
-        console.log(response.data);
-        localStorage.setItem("token", response.data.access);
-        window.location = "/login";
-    }
-    let handleSubmitSignUp = async (event) => {
-        event.preventDefault();
-        let response = await axios.post("http://127.0.0.1:8000/api/signup");
-        console.log(response.data);
-        localStorage.setItem("token", response.data.access);
-        window.location = "/signup";
-    }
     return(
         <div className="LandingPageParent">
             <div className="SlideOne">
                 <div className="SlideOne-text">
                     <h1 className="welcome">For the love of stories.</h1>
+                    <h4 className="slogan">Read. Share. Explore.</h4>
                 </div>
                 <div class="HomeButtons">
                     <a className="LogInButton" href="/login">Log In</a>
