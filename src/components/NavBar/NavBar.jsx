3bb3591e95aa0ce76b695768ad5df98998b658bf
Mitch Anderson <67846917+MitchA29@users.noreply.github.com>
2022-01-03 15:03:56 -0600

    edited navbar to hav logoutoption.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,36 +2,48 @@ import "./NavBar.css";
 import React, { useState } from 'react';
 import { NavLink } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { Nav } from "react-bootstrap";
 
 const NavBar = ({ user }) => {
     return (
-        <nav className="navbar">
-            <Link to="/" className="nav-logo" >
-                Dewey
-            </Link>
-            <ul className='nav-links'>
-                <li className="nav-item">
-                    <Link to="/" className="nav-link" >
-                        Home
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/explore" className="nav-link" >
-                        Explore
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/bookclubs" className="nav-link" >
-                        Book Clubs
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/profile" className="nav-link" >
-                        Profile
-                    </Link>
-                </li>
-            </ul>
-        </nav>
+        <Nav className="navbar">
+            {!user && (
+                <>
+                </>
+            )}
+            {user && (
+                <>
+                    <Nav.Link to="/" className="nav-logo" >
+                        Dewey
+                    </Nav.Link>
+                    <ul className="nav-links">
+                        <li className="nav-item">
+                            <Nav.Link to="/" className="nav-link" >
+                                Home
+                            </Nav.Link>
+                        </li>
+                        <li className="nav-item">
+                            <Nav.Link to="/explore" className="nav-link" >
+                                Explore
+                            </Nav.Link>
+                        </li>
+                        <li className="nav-item">
+                            <Nav.Link to="/bookclubs" className="nav-link" >
+                                Book Clubs
+                            </Nav.Link>
+                        </li>
+                        <li className="nav-item">
+                            <Nav.Link to="/profile" className="nav-link" >
+                                Profile
+                            </Nav.Link>
+                        </li>
+                        <li className="nav-item">
+                            <Nav.Link href="/">Logout</Nav.Link>
+                        </li>
+                    </ul>
+                </>
+            )}
+        </Nav>
     );
 }
 export default NavBar;
