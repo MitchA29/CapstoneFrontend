00172d393397e175d1a0e6d63ef61c3a3ae48439
Mitch Anderson <67846917+MitchA29@users.noreply.github.com>
2022-01-03 20:33:45 -0600

    Edited card layout for stories.

--- src/components/Explore/Explore.jsx
+++ src/components/Explore/Explore.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import {Card} from "react-bootstrap";
+import "./Explore.css";
+import { Container, Row, Col } from "reactstrap";
 
 function Explore(){
     const [stories, setStories] = useState([])
@@ -19,17 +21,21 @@ function Explore(){
 
     return(
         <div className="exploreParent">
-            {stories.map((getStories)=>
-            <Card className="card mb-3">
-                <Card.Body>
-                    <h4 className="card-title">{getStories.storyName}</h4>
-                    <h5 className="random">By: {getStories.storyAuthor_id.first_name}</h5>
-                    <h6 className="random">Genre: {getStories.storyGenre}</h6>
-                    <p className= "card-text">{getStories.storyDescription}</p>
-                    <a href="#" className="btn btn-primary">Download</a>
-                </Card.Body>
-            </Card>
-            )}
+            <container>
+                <Row>
+                {stories.map((getStories)=>
+                    <Card className="storyCard">
+                        <Card.Body>
+                            <h4 className="card-title">{getStories.storyName}</h4>
+                            <h5 className="random">By: {getStories.storyAuthor_id.first_name}</h5>
+                            <h6 className="random">Genre: {getStories.storyGenre}</h6>
+                            <p className= "card-text">{getStories.storyDescription}</p>
+                            <a href="#" className="btn btn-primary">Download</a>
+                        </Card.Body>
+                    </Card>
+                    )}
+                </Row>
+            </container>
         </div>
     )
 }
