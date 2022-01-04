17fae009608b1de5dc5c257f9fb417c90cff2da2
Mitch Anderson <67846917+MitchA29@users.noreply.github.com>
2022-01-04 02:27:31 -0600

    Edited styling of Profile Component.

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -6,7 +6,7 @@ import { Container, Row, Col } from "reactstrap";
 
 function Profile(){
     const [stories, setStories] = useState([])
-    const [Clubs, setClubs] = useState([])
+    const [clubs, setClubs] = useState([])
     const token = localStorage.getItem('token');
     useEffect(()=> {
         getStories()
@@ -29,48 +29,58 @@ function Profile(){
 
     return(
         <div className="profileParent">
-            <div className="profileheader">
-            <a className="uploadButton" href="/upload">Upload</a>
-            <a className="createClubButton" href="/createclub">Create Club</a>
-            </div>
-            <div className="exploreParent">
-            <container>
-            <div>
-                <Row>
-                {stories.map((getStories)=>
-                <Col xs="4">
-                    <Card className="storyCard">
-                        <Card.Body>
-                            <h4 className="card-title">{getStories.storyName}</h4>
-                            <h5 className="random">By: {getStories.storyAuthor_id.first_name}</h5>
-                            <h6 className="random">Genre: {getStories.storyGenre}</h6>
-                            <p className= "card-text">{getStories.storyDescription}</p>
-                            <a href="#" className="btn btn-primary">Download</a>
-                        </Card.Body>
-                    </Card>
-                    </Col>
-                    )}
-                </Row>
+            <div className="storyHeader">
+                <div className="storyHeaderTitle">
+                    <h1>My Stories</h1>
                 </div>
-            </container>
+                <a className="uploadButtonProfile" href="/upload">Upload</a>
             </div>
-            <div className="bookClubsParent">
-            <container>
-                <Row>
-                {Clubs.map((getClubs)=>
-                <Col xs="4">
-            <Card className="clubCard">
-                <Card.Body>
-                    <h4 className="card-title">{getClubs.clubName}</h4>
-                    <h5 className="random">ringleader: {getClubs.clubAuthor_id}</h5>
-                    <p className= "card-text">{getClubs.clubDescription}</p>
-                    <a href="#" className="btn btn-primary">Join</a>
-                </Card.Body>
-            </Card>
-            </Col>
-            )}
-                </Row>
-            </container>
+            <div className="exploreParentProfile">
+                <container>
+                    <div>
+                        <Row>
+                        {stories.map((getStories)=>
+                        <Col xs="4">
+                            <Card className="storyCardProfile">
+                                <Card.Body>
+                                    <h4 className="card-title">{getStories.storyName}</h4>
+                                    <h5 className="random">By: {getStories.storyAuthor_id.first_name}</h5>
+                                    <h6 className="random">Genre: {getStories.storyGenre}</h6>
+                                    <p className= "card-text">{getStories.storyDescription}</p>
+                                    <a href="#" className="btn btn-primary">Download</a>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        )}
+                        </Row>
+                    </div>
+                </container>
+            </div>
+            <div className="clubsHeader">
+                <div className="clubsHeaderTitle">
+                    <h1>My clubs</h1>
+                </div>
+                <a className="createClubButtonProfile" href="/createclub">Create Club</a>
+            </div>
+            <div className="bookClubsParentProfile">
+                <container>
+                    <div>
+                        <Row>
+                        {clubs.map((getClubs)=>
+                        <Col xs="4">
+                            <Card className="clubCardProfile">
+                                <Card.Body>
+                                    <h4 className="card-title">{getClubs.clubName}</h4>
+                                    <h5 className="random">ringleader: {getClubs.clubAuthor_id}</h5>
+                                    <p className= "card-text">{getClubs.clubDescription}</p>
+                                    <a href="#" className="btn btn-primary">Join</a>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        )}
+                        </Row>
+                    </div>
+                </container>
             </div> 
         </div>
     );
