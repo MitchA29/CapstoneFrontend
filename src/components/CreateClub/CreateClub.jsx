2089f57aad21aafb47bbef8e5e8679e2f5e3c579
Mitch Anderson <67846917+MitchA29@users.noreply.github.com>
2022-01-04 01:13:10 -0600

    Modified CreateClub form to post to backend.

--- src/components/CreateClub/CreateClub.jsx
+++ src/components/CreateClub/CreateClub.jsx
@@ -5,75 +5,53 @@ import jwtDecode from "jwt-decode";
 
 
 const CreateClub = () => {
-    const [storyDocument, setStoryDocument] = useState("");
-    const [storyName, setStoryName] = useState("");
-    const [storyDescription, setStoryDescription] = useState("");
-    const [storyGenre, setStoryGenre] = useState("");
+    const [clubName, setClubName] = useState("");
+    const [clubDescription, setClubDescription] = useState("");
+    const [clubBook, setClubBook] = useState("");
 
-    let newStory = {
-        storyDocument: storyDocument,
-        storyName: storyName,
-        storyDescription: storyDescription,
-        storyGenre: storyGenre,
+    let newClub = {
+        clubName: clubName,
+        clubDescription: clubDescription,
+        clubBook: clubBook,
     }
 
     let handleSubmit = async (event) => {
         const token = localStorage.getItem('token');
         console.log(jwtDecode(token))
         event.preventDefault();
-        newStory = {
-        storyDocument: storyDocument,
-        storyName: storyName,
-        storyDescription: storyDescription,
-        storyGenre: storyGenre,
-        author: jwtDecode(token).user_id
+        newClub = {
+        clubName: clubName,
+        clubDescription: clubDescription,
+        clubBook: clubBook,
+        creator: jwtDecode(token).user_id
         }
        
     
     console.log(token)
-    let response = await axios.post("http://127.0.0.1:8000/api/stories/", newStory, {
+    let response = await axios.post("http://127.0.0.1:8000/api/clubs/", newClub, {
         headers: {
             Authorization: 'Bearer ' + token}});
         console.log(response.data);
     }
 
     return (
-        <div className="uploadParent">
-            <div className="uploadForm">
+        <div className="createClubParent">
+            <div className="createClubForm">
                 <form class="row g-3" onSubmit={(event) => handleSubmit(event)}>
                     <div class="col-md-5">
-                        <label for="formFileSm" class="form-label" onChange={(event) => setStoryDocument(event.target.value)}>Document</label>
-                        <input class="form-control" type="file" id="formFile"/>
+                        <label for="inputClubName" class="form-label">Club Name</label>
+                        <input type="text" class="form-control" id="inputStoryName"onChange={(event) => setClubName(event.target.value)}/>
                         </div>
                     <div class="col-md-5">
-                        <label for="inputStoryName" class="form-label">Title</label>
-                        <input type="text" class="form-control" id="inputStoryName"onChange={(event) => setStoryName(event.target.value)}/>
+                        <label for="inputClubBook" class="form-label">Club Book</label>
+                        <input type="text" class="form-control" id="inputStoryName"onChange={(event) => setClubBook(event.target.value)}/>
                     </div>
                     <div class="col-md-5">
-                        <label for="inputStoryDescription" class="form-label">Description</label>
-                        <input type="text" class="form-control" id="inputStoryDescription" onChange={(event) => setStoryDescription(event.target.value)}/>
-                    </div>
-                    <div class="col-md-5">
-                    <select class="form-select" aria-label="Genre" id="inputStoryGenre" onChange={(event) => setStoryGenre(event.target.value)}>
-                        <option selected>Genre</option>
-                        <option value="1">Action and Aventure</option>
-                        <option value="2">Classics</option>
-                        <option value="3">Comic/Graphic</option>
-                        <option value="4">Mystery</option>
-                        <option value="5">Fantasy</option>
-                        <option value="6">Historical Fiction</option>
-                        <option value="7">Horror</option>
-                        <option value="8">Literary Fiction</option>
-                        <option value="9">Romance</option>
-                        <option value="10">Science Fiction</option>
-                        <option value="11">Short Story</option>
-                        <option value="12">Suspence/Thriller</option>
-                        <option value="13">Biographies and Autobiographies</option>
-                        <option value="14">Other</option>
-                    </select>
+                        <label for="inputClubDescription" class="form-label">Description</label>
+                        <input type="text" class="form-control" id="inputStoryDescription" onChange={(event) => setClubDescription(event.target.value)}/>
                     </div>
                     <div class="col-12">
-                        <button type="submit" id="uploadButton" class="uploadButton">Upload</button>
+                        <button type="submit" id="createClubButton" class="createClubButton">Create</button>
                     </div>
                 </form>
             </div>
