5f01ef3ba8164ef8a7a71b787435128487c4b4bc
Mitch Anderson <67846917+MitchA29@users.noreply.github.com>
2022-01-11 04:35:27 -0600

    Created a filter tool for stories.

--- src/components/Explore/Explore.jsx
+++ src/components/Explore/Explore.jsx
@@ -6,8 +6,10 @@ import { Container, Row, Col } from "reactstrap";
 
 function Explore(){
     const [stories, setStories] = useState([])
+    const [filteredStories, setFilteredStories] = useState([])
     useEffect(()=>{
         getStories()
+        getFilteredStories()
     },[])
 
     const getStories = async () =>{
@@ -15,26 +17,15 @@ function Explore(){
         setStories(response.data)
     }
 
+    const getFilteredStories = async (event) => {
+        console.log(event)
+        let filtered = stories.filter(s => s.storyGenre === event)
+        setFilteredStories(filtered)
+    }
+
     return(
         <div className="exploreParent">
-            {/* <select class="form-select" aria-label="Default select example">
-            <option selected>Filter By Genre</option>
-                        <option value="Action and Aventure">Action and Aventure</option>
-                        <option value="Classics">Classics</option>
-                        <option value="Comic/Graphic">Comic/Graphic</option>
-                        <option value="Mystery">Mystery</option>
-                        <option value="Fantasy">Fantasy</option>
-                        <option value="Historical Fiction">Historical Fiction</option>
-                        <option value="Horror">Horror</option>
-                        <option value="Literary Fiction">Literary Fiction</option>
-                        <option value="Romance">Romance</option>
-                        <option value="Science Fiction">Science Fiction</option>
-                        <option value="Short Story">Short Story</option>
-                        <option value="Suspence/Thriller">Suspence/Thriller</option>
-                        <option value="Biographies and Autobiographies">Biographies and Autobiographies</option>
-                        <option value="Other">Other</option>
-            </select> */}
-            <container>
+            <Container className="exploreAll">
             <div>
                 <Row>
                 {stories.map((getStories)=>
@@ -52,7 +43,45 @@ function Explore(){
                     )}
                 </Row>
                 </div>
-            </container>
+            </Container>
+            <div className="exploreFilter">
+            <select className="filter" aria-label="Default select example" onChange={(event) => getFilteredStories(event.target.value)}>
+            <option defaultValue>Filter By Genre</option>
+                        <option value="Action and Aventure">Action and Aventure</option>
+                        <option value="Classics">Classics</option>
+                        <option value="Comic/Graphic">Comic/Graphic</option>
+                        <option value="Mystery">Mystery</option>
+                        <option value="Fantasy">Fantasy</option>
+                        <option value="Historical Fiction">Historical Fiction</option>
+                        <option value="Horror">Horror</option>
+                        <option value="Literary Fiction">Literary Fiction</option>
+                        <option value="Romance">Romance</option>
+                        <option value="Science Fiction">Science Fiction</option>
+                        <option value="Short Story">Short Story</option>
+                        <option value="Suspence/Thriller">Suspence/Thriller</option>
+                        <option value="Biographies and Autobiographies">Biographies and Autobiographies</option>
+                        <option value="Other">Other</option>
+            </select>
+            <Container className="exploreAll">
+            <div>
+                <Row>
+                {filteredStories.map((getFilteredStories)=>
+                <Col xs="4">
+                    <Card className="storyCard" key={Math.random()}>
+                        <Card.Body>
+                            <h4 className="card-title">{getFilteredStories.storyName}</h4>
+                            <h5 className="random">By: {getFilteredStories.storyAuthor_id.first_name}</h5>
+                            <h6 className="random">Genre: {getFilteredStories.storyGenre}</h6>
+                            <p className= "card-text">{getFilteredStories.storyDescription}</p>
+                            <a href="#" className="btn btn-primary">Download</a>
+                        </Card.Body>
+                    </Card>
+                    </Col>
+                    )}
+                </Row>
+                </div>
+            </Container>
+            </div>
         </div>
     )
 }
